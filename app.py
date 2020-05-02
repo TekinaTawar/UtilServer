b8f2f960f8313976b299c509db7dc1dc86551281
TekinaTawar <astroaniket1@gmail.com>
2020-05-02 21:30:22 +0530

    changed api to app

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-
+import json
 import lambda_function
 
 app = Flask(__name__)
@@ -11,7 +11,7 @@ class SendEmail(Resource):
     def post(self):
         data = request.get_json()
         print(lambda_function.sendEmail(data))
-        return {"status": 200}
+        return json.dumps({"status": 200})
 
 
 api.add_resource(SendEmail, '/sendemail')
