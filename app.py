514b780c6bb774634546f343ee19b35266c420bc
TekinaTawar <astroaniket1@gmail.com>
2020-05-03 19:31:24 +0530

    added updating xlsx file

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 
 import lambda_function
+import survey_con
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,6 +15,7 @@ class SendEmail(Resource):
     def post(self):
         data = request.get_json()
         print(lambda_function.sendEmail(data))
+        survey_con.convert_it()
         return {"status": 200}
 
 
