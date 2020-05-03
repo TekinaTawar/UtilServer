8fe415952e4ebac7eb72b1cab068494573c08e38
TekinaTawar <astroaniket1@gmail.com>
2020-05-03 20:31:00 +0530

    Revert "added updating xlsx file"
    This reverts commit 514b780c6bb774634546f343ee19b35266c420bc.

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 
 import lambda_function
-import survey_con
 
 app = Flask(__name__)
 api = Api(app)
@@ -15,7 +14,6 @@ class SendEmail(Resource):
     def post(self):
         data = request.get_json()
         print(lambda_function.sendEmail(data))
-        survey_con.convert_it()
         return {"status": 200}
 
 
